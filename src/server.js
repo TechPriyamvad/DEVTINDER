3f3a3ab5fe3dd82cf745b82e7c8af8acdfb6ab79
TechPriyamvad <priyamvadofficial1@gmail.com>
2026-04-23 07:14:43 +0530

    added changes for encrypting password homework

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 // creating a basic web server using express
 const express = require('express')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 const connectDb = require('./configs/db')
 const {userModel,userTypes} = require('./models/user')
 const { USER1 } = require('./constants/user')
 const app = express()
 const PORT = 3000
+const saltRounds = 12
 
  // Middleware to parse JSON bodies
 app.use(express.json())
@@ -15,7 +17,6 @@ app.post('/signup', async (req, res) => {
     try {
         const userJson = req.body
         const {email,password} = userJson
-
         // valid email check
         if(!validator.isEmail(email)){
             return res.status(400).json({message:"Invalid email format"})
@@ -30,8 +31,9 @@ app.post('/signup', async (req, res) => {
         if(checkUser){
             return res.status(400).json({message:"User with email " + email + " already exists"})
         }
-
         
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
+        userJson.password = hashedPassword
         const user = new userModel(userJson)
         await user.save({validateBeforeSave:true})
         res.status(201).json({ message: "User created successfully", user })
@@ -120,7 +122,28 @@ app.delete('/userById/:id',async (req,res)=>{
     }
 })
 
+// login api
+app.post('/login',async (req,res)=>{
+    try {
+        const {email,password} = req.body
 
+        // check if email exists
+        const user = await userModel.findOne({email: email})
+        if(!user){
+            return res.status(401).json({message:"Invalid credentials"})
+        }
+
+        // compare hashed password with the password provided by user
+        const match = await bcrypt.compare(password, user.password)
+        if(!match){
+            return res.status(401).json({message:"Invalid credentials"})
+        }
+        res.status(200).json({message:"User logged in successfully"})
+
+    } catch (error) {
+        res.status(500).json({ message: "Error logging user", error:error.message, stack: error.stack })
+    }
+})
 // starting the server
 connectDb().then(() => {
     console.log("Database connected successfully")
